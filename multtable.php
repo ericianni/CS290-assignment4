2ff49cc548dcd441cf0796172041d51e72db25e1
Eric Ianni <iannie@onid.oregonstate.edu>
2015-02-05 09:37:42 -0500

    During testing realized the assignment said the min and max are allowed to be equal.

--- multtable.php
+++ multtable.php
@@ -5,91 +5,83 @@ ini_set('display_errors', 'On');
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Form Practice</title>
+	<title>multtable.php</title>
 	<link rel="stylesheet" type="text/css" href="multtable.css">
 </head>
-<body>
-<form action="multtable.php" method="get">
-	What is the Minimum Multiplicand: <input type="number" name="min-multiplicand" value="" /><br />
-	What is the Maximum Multiplicand: <input type="number" name="max-multiplicand" value="" /><br />
-	What is the Minimum Multiplier: <input type="number" name="min-multiplier" value="" /><br />
-	What is the Maximum Multiplier: <input type="number" name="max-multiplier" value="" /><br />
-	<input type="submit" value="Submit" \>
-</form>
 <?php 
 	
 	$valid = true;
-	$minCand = (int)$_GET['min-multiplicand'];
-	$maxCand = (int)$_GET['max-multiplicand'];
-	$minMult = (int)$_GET['min-multiplier'];
-	$maxMult = (int)$_GET['max-multiplier'];
+	if(isset($_GET['min-multiplicand']) === false) {
+		echo "Missing parameter [min-multiplicand]<br />";
+		$valid = false;
+	} else {
+		if((int)$_GET['min-multiplicand'] == NULL) {
+			echo "[min-multiplicand] must be an integer<br />";
+			$valid = false;
+		} else {
+			$minCand = $_GET['min-multiplicand'];
+		}
+	}
+	
+	if(isset($_GET['max-multiplicand']) === false) {
+		echo "Missing parameter [max-multiplicand]<br />";
+		$valid = false;
+	} else {
+		if((int)$_GET['max-multiplicand'] == NULL) {
+			echo "[max-multiplicand] must be an integer<br />";
+			$valid = false;
+		} else {
+			$maxCand = $_GET['max-multiplicand'];
+		}
+	}
 
+	if(isset($_GET['min-multiplier']) == false) {
+		echo "Missing parameter [min-multiplier]<br />";
+		$valid = false;
+	} else {
+		if((int)$_GET['min-multiplier'] == NULL) {
+			echo "[min-multiplier] must be an integer<br />";
+			$valid = false;
+		} else {
+			$minMult = $_GET['min-multiplier'];
+		}
+	}
+	
+		if(isset($_GET['max-multiplier']) == false) {
+		echo "Missing parameter [max-multiplier]<br />";
+		$valid = false;
+	} else {
+		if((int)$_GET['max-multiplier'] == NULL) {
+			echo "[max-multiplier] must be an integer<br />";
+			$valid = false;
+		} else {
+			$maxMult = $_GET['max-multiplier'];
+		}
+	}
+	if(!$valid) {
+		echo "Can't proceed due to errors in input. See Above.<br />";
+		exit();
+	}
+
+	if($minCand > $maxCand) {
+		echo "min-multiplicand must be less than max-multiplicand <br />";
+		$valid = false;
+	}
+	if($minMult > $maxMult) {
+		echo "min-multiplier must be less than max-multiplier <br />";
+		$valid = false;
+	}
+
+	if(!$valid) {
+		echo "Can't proceed due to errors in input. See Above.<br />";
+		exit();
+	}
 	echo "The Minimum Multiplicand is: ".$minCand."<br />";
 	echo "The Maximum Multiplicand is: ".$maxCand."<br />";
 	echo "The Minimum Multiplier is: ".$minMult."<br />";
 	echo "The Maximum Multiplier is: ".$maxMult."<br />";
 
-			echo gettype($minCand);
-	if(gettype($minCand) != "integer") {
 
-		echo "The Minimum Multiplicand must be an integer<br />";
-		$valid = false;
-	}
-
-	if(gettype($maxCand) != "integer") {
-		echo "The Maximum Multiplicand must be an integer<br />";
-		$valid = false;
-	}
-	
-	if(gettype($minMult) != "integer") {
-		echo "The Minimum Multiplier must be an integer<br />";
-		$valid = false;
-	}
-
-	if(gettype($maxMult) != "integer") {
-		echo "The Minimum Multiplier must be an integer<br />";
-		$valid = false;
-	}
-
-	if($minCand >= $maxCand) {
-		echo "min-multiplicand not less than max-multiplicand<br />";
-		$valid = false;
-	}
-
-	if($minMult >= $maxMult) {
-		echo "min-multiplier not less than max-multiplier<br />";
-		$valid = false;
-	}
-
-	if($minCand == NULL || $maxCand == NULL || $minMult == NULL || $maxMult == NULL) {
-		
-		if($minCand == NULL) {
-			echo "Missing parameter [min-multiplicand]<br />";
-		}
-		if($maxCand == NULL) {
-			echo "Missing parameter [min-multiplicand]<br />";
-		}
-		if($minMult == NULL) {
-			echo "Missing parameter [min-multiplicand]<br />";
-		}
-		if($maxMult == NULL) {
-			echo "Missing parameter [min-multiplicand]<br />";
-		}
-		$valid = false;
-	}
-
-	if($minCand >= $maxCand) {
-		echo "min-multiplicand not less than max-multiplicand<br />";
-		$valid = false;
-	}
-	if($minMult >= $maxMult) {
-		echo "min-multiplier not less than max-multiplier<br />";
-		$valid = false;
-	}
-
-	if($valid === false) {
-		exit();
-	}
 
 	echo "<table id='phpTable'>";
 	echo "<thead>";
